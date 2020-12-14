fbfa1cbba46ebbec7b4d39616faaa8dbee505025
Elisei Nicolae <43387542+Elisei123@users.noreply.github.com>
2020-12-14 10:02:47 +0200

    create new GUI

--- src/com/company/GUI_Form.java
+++ src/com/company/GUI_Form.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 public class GUI_Form extends JFrame {
     private JPanel rootPanel;
+    private JPanel problemInterface;
     private JButton dumitruAndreiValentinButton;
     private JButton aldescuAlinButton;
     private JButton harcanAdrianButton;
@@ -13,6 +14,15 @@ public class GUI_Form extends JFrame {
     private JButton mihalacheDavidAlexandruButton;
     private JButton miloiuMarianIulianButton;
     private JButton nicolaeEliseiButton;
+    private JTextArea inputTextArea;
+    private JTextArea outputTextArea;
+    private JButton baciuAlexandruButton;
+    private JLabel studentName;
+    private JLabel descriptionStudentName;
+    private JLabel descriptionNameProblem;
+    private JLabel problemName;
+    private JLabel inputDescription;
+    private JLabel outputDescription;
 
 
     public GUI_Form(){
@@ -20,20 +30,35 @@ public class GUI_Form extends JFrame {
         add(rootPanel);
 
         setTitle("ISW Program");
-        setSize(400, 600);
+        setSize(1200, 600);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        studentName.setText("");
+        studentName.setText("               ");
+        problemName.setText("               ");
+        descriptionNameProblem.setText("Cerinta problema:");
+        descriptionNameProblem.setVisible(false);
+        descriptionStudentName.setText("Nume student:");
+        descriptionStudentName.setVisible(false);
+        inputTextArea.setVisible(false);
+        outputTextArea.setVisible(false);
+        inputDescription.setVisible(false);
+        outputDescription.setVisible(false);
 
         nicolaeEliseiButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPanel, "Nicolae Elisei");
+                studentName.setText("Nicolae Elisei");
+                problemName.setText("50.    Să se calculeze înălțimea unui arbore și să să insereze valoarea respectivă în locul tuturor valorilor nule din arbore.");
+                setPanelVisible();
             }
         });
 
         dumitruAndreiValentinButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPanel, "dumitruAndreiValentinButton");
+                studentName.setText("Dumitru Andrei Valentin");
+                problemName.setText("numar problema Andrei");
+                setPanelVisible();
             }
         });
 
@@ -41,7 +66,9 @@ public class GUI_Form extends JFrame {
         aldescuAlinButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPanel, "aldescuAlinButton");
+                studentName.setText("Aldescu Alin");
+                problemName.setText("Nr prb Alin");
+                setPanelVisible();
             }
         });
 
@@ -49,7 +76,9 @@ public class GUI_Form extends JFrame {
         harcanAdrianButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPanel, "harcanAdrianButton");
+                studentName.setText("Harcan Adrian");
+                problemName.setText("Nr prb Adrian");
+                setPanelVisible();
             }
         });
 
@@ -57,14 +86,18 @@ public class GUI_Form extends JFrame {
         carstoceaCosminMarianButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPanel, "carstoceaCosminMarianButton");
+                studentName.setText("Carstocea Cosmin Marian");
+                problemName.setText("Nr prb Cosmin");
+                setPanelVisible();
             }
         });
 
         mihalacheDavidAlexandruButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPanel, "mihalacheDavidAlexandruButton");
+                studentName.setText("Mihalache David Alexandru");
+                problemName.setText("Nr prb David");
+                setPanelVisible();
             }
         });
 
@@ -72,8 +105,27 @@ public class GUI_Form extends JFrame {
         miloiuMarianIulianButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPanel, "miloiuMarianIulianButton1");
+                studentName.setText("Miloiu Marian Iulian");
+                problemName.setText("Nr prb Iulian");
+                setPanelVisible();
             }
         });
+        baciuAlexandruButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                studentName.setText("Baciu Alexandru");
+                problemName.setText("Nr prb Alexandru");
+                setPanelVisible();
+            }
+        });
+    }
+
+    public void setPanelVisible(){
+        descriptionNameProblem.setVisible(true);
+        descriptionStudentName.setVisible(true);
+        inputTextArea.setVisible(true);
+        outputTextArea.setVisible(true);
+        inputDescription.setVisible(true);
+        outputDescription.setVisible(true);
     }
 }
